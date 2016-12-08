f1fc82d9c470531a7262cd12783f3a9a957f50a2
raul naupari <raul.naupari@cignium.com>
2016-12-08 14:26:14 -0500

    test

--- coverage.bat
+++ coverage.bat
@@ -1,3 +1,3 @@
 .\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:.\packages\NUnit.Runners.2.6.4\tools\nunit-console.exe -targetargs:"/nologo /noshadow .\Jal.Factory.Tests\bin\Debug\Jal.Factory.Tests.dll" -filter:"+[Jal.*]* -[*.Tests]*" -register:user
 
-.\packages\coveralls.net.0.7.0\tools\csmacnz.Coveralls.exe --opencover -i .\results.xml
+.\packages\coveralls.io.1.3.4\tools\coveralls.net.exe --opencover .\results.xml
