3bdb8c1f460d564c753e8b30fcc3bc967d55ffbb
raul naupari <raul.naupari@cignium.com>
2018-05-28 13:29:29 -0500

    cleanup

--- Jal.Factory.Tests/CastleWindsor/Tests.cs
+++ Jal.Factory.Tests/CastleWindsor/Tests.cs
@@ -1,12 +1,16 @@
-﻿using Castle.MicroKernel.Registration;
+﻿using System;
+using Castle.MicroKernel.Registration;
 using Castle.MicroKernel.Resolvers.SpecializedResolvers;
 using Castle.Windsor;
+using Jal.Factory.Impl;
 using Jal.Factory.Installer;
 using Jal.Factory.Interface;
 using Jal.Factory.Tests.Impl;
 using Jal.Factory.Tests.Interfaces;
 using Jal.Factory.Tests.Model;
+using Jal.Finder.Atrribute;
 using Jal.Locator.CastleWindsor.Installer;
+using Jal.Locator.Impl;
 using NUnit.Framework;
 using Shouldly;
 
@@ -17,7 +21,41 @@ namespace Jal.Factory.Tests.CastleWindsor
     {
 
         [Test]
-        public void Create_WithCustomerOlderThan25_ShouldBeNotEmpty()
+        public void Create_WithCustomerOlderThan25A_ShouldBeNotEmpty()
+        {
+            var container = new WindsorContainer();
+
+            var directory = AppDomain.CurrentDomain.BaseDirectory;
+
+            var finder = Finder.Impl.AssemblyFinder.Builder.UsePath(directory).Create;
+
+            container.Kernel.Resolver.AddSubResolver(new ArrayResolver(container.Kernel));
+
+            container.Install(new ServiceLocatorInstaller());
+
+            var assemblies = finder.GetAssembliesTagged<AssemblyTagAttribute>();
+
+            container.Install(new FactoryInstaller(assemblies));
+
+            container.Register(Component.For<IDoSomething>().ImplementedBy<DoSomething>().LifestyleSingleton().Named(typeof(DoSomething).FullName));
+
+            var factory = container.Resolve<IObjectFactory>();
+
+            var customer = new Customer(){Age = 25};
+
+            var services = factory.Create<Customer, IDoSomething>(customer);
+
+            services.ShouldNotBeEmpty();
+
+            services.Length.ShouldBe(1);
+
+            services[0].ShouldBeAssignableTo<IDoSomething>();
+
+            services[0].ShouldBeOfType<DoSomething>();
+        }
+
+        [Test]
+        public void Create_WithCustomerOlderThan25B_ShouldBeNotEmpty()
         {
             var container = new WindsorContainer();
 
@@ -25,13 +63,13 @@ namespace Jal.Factory.Tests.CastleWindsor
 
             container.Install(new ServiceLocatorInstaller());
 
-            container.Install(new FactoryInstaller(new IObjectFactoryConfigurationSource[]{ new AutoObjectFactoryConfigurationSource() } ));
+            container.Install(new FactoryInstaller(new IObjectFactoryConfigurationSource[] { new AutoObjectFactoryConfigurationSource() }));
 
             container.Register(Component.For<IDoSomething>().ImplementedBy<DoSomething>().LifestyleSingleton().Named(typeof(DoSomething).FullName));
 
             var factory = container.Resolve<IObjectFactory>();
 
-            var customer = new Customer(){Age = 25};
+            var customer = new Customer() { Age = 25 };
 
             var services = factory.Create<Customer, IDoSomething>(customer);
 
--- Jal.Factory.Tests/LightInject/Tests.cs
+++ Jal.Factory.Tests/LightInject/Tests.cs
@@ -1,8 +1,11 @@
-﻿using Jal.Factory.Interface;
+﻿using System;
+using Jal.Factory.Interface;
 using Jal.Factory.LightInject.Installer;
 using Jal.Factory.Tests.Impl;
 using Jal.Factory.Tests.Interfaces;
 using Jal.Factory.Tests.Model;
+using Jal.Finder.Atrribute;
+using Jal.Finder.Impl;
 using Jal.Locator.LightInject.Installer;
 using LightInject;
 using NUnit.Framework;
@@ -15,7 +18,39 @@ namespace Jal.Factory.Tests.LightInject
     {
 
         [Test]
-        public void Create_WithCustomerOlderThan25_ShouldBeNotEmpty()
+        public void Create_WithCustomerOlderThan25A_ShouldBeNotEmpty()
+        {
+            var container = new ServiceContainer();
+
+            var directory = AppDomain.CurrentDomain.BaseDirectory;
+
+            var finder = AssemblyFinder.Builder.UsePath(directory).Create;
+
+            container.RegisterFrom<ServiceLocatorCompositionRoot>();
+
+            var assemblies = finder.GetAssembliesTagged<AssemblyTagAttribute>();
+
+            container.RegisterFactory(assemblies);
+
+            container.Register<IDoSomething, DoSomething>(typeof(DoSomething).FullName);
+
+            var factory = container.GetInstance<IObjectFactory>();
+
+            var customer = new Customer(){Age = 25};
+
+            var services = factory.Create<Customer, IDoSomething>(customer);
+
+            services.ShouldNotBeEmpty();
+
+            services.Length.ShouldBe(1);
+
+            services[0].ShouldBeAssignableTo<IDoSomething>();
+
+            services[0].ShouldBeOfType<DoSomething>();
+        }
+
+        [Test]
+        public void Create_WithCustomerOlderThan25B_ShouldBeNotEmpty()
         {
             var container = new ServiceContainer();
 
@@ -27,7 +62,7 @@ namespace Jal.Factory.Tests.LightInject
 
             var factory = container.GetInstance<IObjectFactory>();
 
-            var customer = new Customer(){Age = 25};
+            var customer = new Customer() { Age = 25 };
 
             var services = factory.Create<Customer, IDoSomething>(customer);
 
